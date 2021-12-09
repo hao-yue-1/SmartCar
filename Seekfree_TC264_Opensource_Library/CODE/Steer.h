54b715ba3ab2ff187b54ef0ecc30a9f74dae7c86
hao-yue-1 <1801134420@qq.com>
2021-12-09 21:17:02 +0800

    修改舵机引脚宏定义

--- Seekfree_TC264_Opensource_Library/CODE/Steer.h
+++ Seekfree_TC264_Opensource_Library/CODE/Steer.h
@@ -10,7 +10,7 @@
 #ifndef CODE_STEER_H_
 #define CODE_STEER_H_
 
-#define MOTOR_PIN   ATOM1_CH1_P33_9     //输出舵机PWM引脚
+#define STEER_PIN   ATOM1_CH1_P33_9     //输出舵机PWM引脚
 #define STEER_MID   750                 //舵机居中值
 #define STEER_LEFT  840                 //舵机向左打死值
 #define STEER_RIGHT 680                 //舵机向右打死值
