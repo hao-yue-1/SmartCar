423f0d8794691793743a53008bfe20e8c2d3b948
hao-yue-1 <1801134420@qq.com>
2021-12-09 15:40:20 +0800

    新增Steer.c/.h舵机控制

---
+++ Seekfree_TC264_Opensource_Library/CODE/Steer.h
@@ -0,0 +1,20 @@
+/*
+ * Steer.h
+ *
+ *  Created on: 2021年12月9日
+ *      Author: yue
+ */
+
+#include "headfile.h"       //逐飞的封装库
+
+#ifndef CODE_STEER_H_
+#define CODE_STEER_H_
+
+#define MOTOR_PIN   ATOM1_CH1_P33_9     //输出舵机PWM引脚
+#define STEER_MID   750                 //舵机居中值
+#define STEER_LEFT  840                 //舵机向左打死值
+#define STEER_RIGHT 680                 //舵机向右打死值
+
+void SteerCtrl(uint16 pwm);     //控制舵机
+
+#endif /* CODE_STEER_H_ */
