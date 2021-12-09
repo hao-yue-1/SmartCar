b4bb67c8330f22bd963745b731c8833a22890f4b
hao-yue-1 <1801134420@qq.com>
2021-12-09 23:38:30 +0800

    新增扫线到赛道外的判断

--- Seekfree_TC264_Opensource_Library/CODE/ImageBasic.c
+++ Seekfree_TC264_Opensource_Library/CODE/ImageBasic.c
@@ -16,7 +16,7 @@ void GetImagBasic(int *LeftLine, int *CentreLine, int *RightLine)
 {
     int row,cloum;   //行,列
     //开始扫线（从下往上，从中间往两边）
-    for(row=0;row<MT9V03X_H;row++) //图像的原点在左下角
+    for(row=0;(row-BORDER_BIAS)<MT9V03X_H;row++) //图像的原点在左下角
     {
         //左边扫线
         for(cloum=mid;(cloum-BORDER_BIAS)>0;cloum--)
@@ -37,7 +37,14 @@ void GetImagBasic(int *LeftLine, int *CentreLine, int *RightLine)
             }
         }
         //数据处理
-        CentreLine[row]=(LeftLine[row]+RightLine[row])/2;   //记录中线
+        CentreLine[row]=(LeftLine[row]+RightLine[row])/2;   //记录中线,列坐标
+        //防止扫线到赛道外
+        if(BinaryImage[row,CentreLine[row]]==0&&BinaryImage[row+BORDER_BIAS,CentreLine[row]]==0)    //row行的中线是黑，扫到了赛道外
+        {
+            if(row>20)  //若还在前20行则忽视,不做break处理
+                break;  //若已经在20行后发现扫描到了赛道外,直接break跳出该图的扫线处理
+        }
+
         mid=CentreLine[row];      //以上一次的中线值为下一次扫线的中间点
     }
 }
