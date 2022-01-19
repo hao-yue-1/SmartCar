6f820d19cd1dc9c63eb7036a995210f14292e46f
hao-yue-1 <1801134420@qq.com>
2022-01-20 00:07:48 +0800

    补充环岛识别的代码，稍微修改一些变量和宏

--- Seekfree_TC264_Opensource_Library/CODE/ImageSpecial.c
+++ Seekfree_TC264_Opensource_Library/CODE/ImageSpecial.c
@@ -40,7 +40,7 @@ uint8 StartLineFlag(Point InflectionL,Point InflectionR)
     int Black_num=0;        //记录行黑线的数量，作为判断该行是否为斑马线的依据
     int Black_times=0;      //记录满足斑马线的行数，并作为判断该路段是否为斑马线的依据
 
-    if(0)    //拐点（车库）在左边
+    if(InflectionL.X!=0&&InflectionL.Y!=0)    //拐点（车库）在左边
     {
         for(row=InflectionL.X,cloum=InflectionL.Y;row<MT9V03X_H;row++)        //从左拐点开始固定列，从下往上扫
         {
@@ -54,13 +54,13 @@ uint8 StartLineFlag(Point InflectionL,Point InflectionR)
                     }
                     else                                        //扫到白点
                     {
-                        if(Black_width>=BLACK_WIDTH)    //判断黑线宽度是否满足阈值
+                        if(Black_width>=S_BLACK_WIDTH)    //判断黑线宽度是否满足阈值
                         {
                             Black_num++; //行黑线数量+1
                         }
                         Black_width=0;   //在一次白点判断后重置黑线宽度
                     }
-                    if(Black_num>=BLACK_NUM)    //若满足斑马线的阈值（这一行）
+                    if(Black_num>=S_BLACK_NUM)    //若满足斑马线的阈值（这一行）
                     {
                         Black_times++;  //满足斑马线的行数+1
                         break;
@@ -68,7 +68,7 @@ uint8 StartLineFlag(Point InflectionL,Point InflectionR)
                 }
                 Black_num=0;    //这一行的扫线结束，重置行黑线数
             }
-            if(Black_times>=BLACK_TIMES)    //若满足斑马线路段的阈值
+            if(Black_times>=S_BLACK_TIMES)    //若满足斑马线路段的阈值
             {
                 return 1;
             }
@@ -76,13 +76,13 @@ uint8 StartLineFlag(Point InflectionL,Point InflectionR)
         return 0;
     }
 
-    if(0)    //拐点（车库）在右边
+    if(InflectionR.X!=0&&InflectionR.Y!=0)    //拐点（车库）在右边
     {
         for(row=InflectionL.X,cloum=InflectionL.Y;row<MT9V03X_H;row++)        //从右拐点开始固定列，从下往上扫
         {
             if(BinaryImage[row][cloum]==IMAGE_BLACK)    //找到了一个黑点
             {
-                for(;cloum>0;cloum--)                                 //固定行，从右往左扫
+                for(;cloum>0;cloum--)                                         //固定行，从右往左扫
                 {
                     if(BinaryImage[row][cloum]==IMAGE_BLACK)    //扫到黑点
                     {
@@ -90,13 +90,13 @@ uint8 StartLineFlag(Point InflectionL,Point InflectionR)
                     }
                     else                                        //扫到白点
                     {
-                        if(Black_width>=BLACK_WIDTH)    //判断黑线宽度是否满足阈值
+                        if(Black_width>=S_BLACK_WIDTH)    //判断黑线宽度是否满足阈值
                         {
                             Black_num++; //行黑线数量+1
                         }
                         Black_width=0;   //在一次白点判断后重置黑线宽度
                     }
-                    if(Black_num>=BLACK_NUM)    //若满足斑马线的阈值（这一行）
+                    if(Black_num>=S_BLACK_NUM)    //若满足斑马线的阈值（这一行）
                     {
                         Black_times++;  //满足斑马线的行数+1
                         break;
@@ -104,32 +104,39 @@ uint8 StartLineFlag(Point InflectionL,Point InflectionR)
                 }
                 Black_num=0;    //这一行的扫线结束，重置行黑线数
             }
-            if(Black_times>=BLACK_TIMES)    //若满足斑马线路段的阈值
+            if(Black_times>=S_BLACK_TIMES)    //若满足斑马线路段的阈值
             {
                 return 2;
             }
         }
         return 0;
     }
+
+    return 0;
 }
 /*
  *******************************************************************************************
  ** 函数功能: 识别环岛
  ** 参    数: LeftLine：左线数组
  **           RightLine：右线数组
+ **           InflectionL：左拐点
+ **           InflectionR：右拐点
  ** 返 回 值: 0：没有识别到环岛
- **           1：识别到环岛且环岛在车身左侧
- **           2：识别到环岛且环岛在车身右侧
+ **           1：识别到环岛且在车身左侧
+ **           2：识别到环岛且在车身右侧
+ **           3：识别到环岛入口且在车身左侧
+ **           4：识别到环岛入口且在车身右侧
  ** 作    者: WBN
  ** 注    意：1 . 默认在进行环岛识别的路段都是直线，即车头已经摆正
  **           2.这个应该是进环岛的函数，出环岛那段应该重写一个，并在进入环岛后再开启出环岛的判断
  ********************************************************************************************
  */
-uint8 RoundaboutFlag(int LeftLine,int RightLine)
+uint8 RoundaboutFlag(int LeftLine,int RightLine,Point InflectionL,Point InflectionR)
 {
     /**
      ** 整个赛道会有3个环岛，一个在起跑线左侧，一个在起跑线右侧，由于是循环赛道所以会存在环岛在车左边和在车右边两种情况；
-     ** 以左环岛为例，若车还没有进入环岛入口，则会检测到
+     ** 以左环岛为例，若车还没有进入环岛入口，则图像会有两种情况：一种是图像中有拐点（即完全在环岛外，这种情况可能会出现直角拐弯的误判，先
+     ** 不做处理）；另一种是图像中没有拐点（既已经行进到环岛一半但还未到达环岛入口）。若车已经到达环岛入口，则只会有一种情况
      ** */
 
     /*
@@ -137,12 +144,51 @@ uint8 RoundaboutFlag(int LeftLine,int RightLine)
      **         就直接退出（这样有一个问题就是在发现环岛入口到发现环岛入口这段时间内，车会以原有的角度与速度行驶）
      ** 方法二：环岛的识别任分发现环岛和到达环岛入口两个部分，函数在发现环岛和到达环岛入口作为两个不同的返回值（加上左右共四种情况即四
      **         个返回值），这样将车子的处理问题抛出，该方法可能更稳定也符合一开始的想法
-     * */
+     ** */
 
-    int row,cloum;          //行,列
+    /*
+     ** 1.识别到环岛：寻找这两种情况的共通点：一边出现丢线，另一边完整，且丢线的一边必定会再次出现线而非全丢
+     ** 1.1 另一种思路：像扫左右边线一样，扫上下边线。。。都是圆好像区别不大？不过下边线可以扫出一条完整的带弧度的线，而左右扫线则可能因为
+     **                 摄像头拍摄不全的缘故扫不到最边缘的线
+     ** 2.识别到入口：一边出现丢线，另一边完整，且丢线的一边必定只是丢了一个缺口而不会有其他情况
+     ** */
 
+    int row,cloum;                      //行,列
+    int white_num=0,black_num=0;        //记录扫线过程的白点数量
+    int times=0;                    //记录符合环岛特征的列数
+    //发现环岛：存在拐点
+    if(InflectionL.X!=0&&InflectionL.Y!=0)   //拐点在左边（发现环岛在车身左侧）
+    {
+        for(row=InflectionL.X,cloum=InflectionL.Y-POINT_MOVE;cloum>0;cloum--)        //外层：从拐点往左扫线
+        {
+            for(;row<MT9V03X_H;row++)                                                //内层：循环是从下往上
+            {
+                if(BinaryImage[row][cloum]==IMAGE_WHITE)    //扫到白点
+                {
+                    white_num++;    //白点数量+1
+                }
+                else                                        //扫到黑点
+                {
+                    black_num++;    //黑点数量+1
+                }
+                if(white_num>=R_WHITE_NUM&&black_num>=R_BLACK_NUM)
+                {
+                    times++;
+                    break;
+                }
+            }
+            if(times>=R_TIMES)
+            {
+                return 1;
+            }
+            white_num=0;
+            black_num=0;
+        }
+    }
 
+    //到达环岛入口：无拐点
 
+    return 0;
 }
 
 /********************************************************************************************
