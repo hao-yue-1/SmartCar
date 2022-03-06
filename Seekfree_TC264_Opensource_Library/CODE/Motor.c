7b454f8c369aa8a44bff2806b83b9c05aefb3600
MrSu <you@example.com>
2022-03-06 16:12:34 +0800

    写了PID初始化函数，配合主控板修改了电机驱动引脚

--- Seekfree_TC264_Opensource_Library/CODE/Motor.c
+++ Seekfree_TC264_Opensource_Library/CODE/Motor.c
@@ -31,13 +31,13 @@ void MotorCtrl(int16 left_speed,int16 right_speed)
     //左电机
     if(left_speed>=0)   //正转
     {
-        pwm_duty(LEFT_MOTOR_PIN1,left_speed);
-        pwm_duty(LEFT_MOTOR_PIN2,0);
+        pwm_duty(LEFT_MOTOR_PIN1,0);
+        pwm_duty(LEFT_MOTOR_PIN2,left_speed);
     }
     else                //反转
     {
-        pwm_duty(LEFT_MOTOR_PIN1,0);
-        pwm_duty(LEFT_MOTOR_PIN2,left_speed);
+        pwm_duty(LEFT_MOTOR_PIN1,left_speed);
+        pwm_duty(LEFT_MOTOR_PIN2,0);
     }
     //右电机
     if(right_speed>=0)  //正转
@@ -59,6 +59,7 @@ void MotorCtrl(int16 left_speed,int16 right_speed)
 ** 形    参: left_encoder：左电机编码器的值
 **           right_encoder：右电机编码器的值
 ** 返 回 值: 无
+** 注    意：由于编码器相对放置，向前运动时对右编码器取反处理
 *********************************************************************************************************
 */
 void MotorEncoder(int16* left_encoder,int16* right_encoder)
@@ -67,7 +68,7 @@ void MotorEncoder(int16* left_encoder,int16* right_encoder)
     *left_encoder = gpt12_get(LEFT_ENCODER);
     gpt12_clear(LEFT_ENCODER);
     //右编码器
-    *right_encoder = gpt12_get(RIGHT_ENCODER);
+    *right_encoder = -gpt12_get(RIGHT_ENCODER);
     gpt12_clear(RIGHT_ENCODER);
 }
 
--- Seekfree_TC264_Opensource_Library/CODE/PID.h
+++ Seekfree_TC264_Opensource_Library/CODE/PID.h
@@ -25,7 +25,7 @@ typedef struct MotorPID
 }MotorPID;
 
 //这里还要写个PID初始化的函数调参在这里进行
-
+void PID_init(SteerPID *SteerK,MotorPID *MotorK);                           //两个PID参数的赋值初始化
 int Steer_Position_PID(float SlopeBias,SteerPID K);                         //舵机位置式PID控制，采用分段式PID控制
 int16 Speed_PI_Left(int16 left_encoder,int16 left_target,MotorPID K);       //电机增量式PI控制 //左电机
 int16 Speed_PI_Right(int16 right_encoder,int16 right_target,MotorPID K);    //电机增量式PI控制  //右电机
