beaa2ae74096d28692433bd99c81dbd1a5b5fa4f
hao-yue-1 <1801134420@qq.com>
2022-04-16 01:04:43 +0800

    完善状态机

--- Seekfree_TC264_Opensource_Library/CODE/ImageProcess.c
+++ Seekfree_TC264_Opensource_Library/CODE/ImageProcess.c
@@ -34,64 +34,60 @@ void ImageProcess()
     /*************************搜寻左右下拐点***********************/
     GetDownInflection(110,45,LeftLine,RightLine,&LeftDownPoint,&RightDownPoint);
     /*************************特殊元素判断*************************/
-    CrossLoopEnd_S();
     /****************************状态机***************************/
-//    switch(flag)
-//    {
-//        case 0: //识别左环岛
-//        {
-//            gpio_set(P21_4, 0);
-//            gpio_set(P21_5, 1);
-//            gpio_set(P20_9, 1);
-//            if(CircleIslandIdentify_L(LeftLine, RightLine, LeftDownPoint, RightDownPoint)==9)
-//            {
-//                flag=1; //跳转到状态1
-//            }
-//            break;
-//        }
-//        case 1: //识别十字回环出口
-//        {
-//            gpio_set(P21_4, 1);
-//            gpio_set(P21_5, 0);
-//            gpio_set(P20_9, 1);
-//            if(CrossLoopEnd(LeftLine, RightLine)==1)
-//            {
-//                flag=2; //跳转到状态2
-//            }
-//            break;
-//        }
-//        case 2: //识别右环岛
-//        {
-//            gpio_set(P21_4, 1);
-//            gpio_set(P21_5, 1);
-//            gpio_set(P20_9, 0);
-//            if(CircleIslandIdentify_R(LeftLine, RightLine, LeftDownPoint, RightDownPoint)==9)
-//            {
-//                flag=3; //跳转到状态3
-//            }
-//            break;
-//        }
-//        case 3: //识别左车库，三岔等
-//        {
-//
-//            break;
-//        }
-//        case 5://识别三岔和右车库
-//        {
-//            if(LostNum_RightLine>40 && LostNum_RightLine<90 && LostNum_LeftLine<10 && LostNum_LeftLine>0)
-//            {
-//                Garage_flag=GarageIdentify('R', LeftDownPoint, RightDownPoint);//识别车库
-//                if(Garage_flag==0)//如果没识别到车库，再继续识别三岔，怕误判,补救
-//                {
-//                    Fork_flag=ForkIdentify(LeftLine, RightLine, LeftDownPoint, RightDownPoint);
-//                }
-//            }
-//            else
-//            {
-//                Fork_flag=ForkIdentify(LeftLine, RightLine, LeftDownPoint, RightDownPoint);
-//            }
-//        }
-//    }
+    switch(flag)
+    {
+        case 0: //识别左环岛
+        {
+            if(CircleIslandIdentify_L(LeftLine, RightLine, LeftDownPoint, RightDownPoint)==9)
+            {
+                flag=1; //跳转到状态1
+            }
+            break;
+        }
+        case 1: //识别第一个十字回环出口
+        {
+            if(CrossLoopEnd_F()==1)
+            {
+                flag=2; //跳转到状态2
+            }
+            break;
+        }
+        case 2: //识别右环岛
+        {
+            if(CircleIslandIdentify_R(LeftLine, RightLine, LeftDownPoint, RightDownPoint)==9)
+            {
+                flag=3; //跳转到状态3
+            }
+            break;
+        }
+        case 3: //识别左车库，三岔等
+        {
+            flag=4; //这里的还没写好，先跳过
+            break;
+        }
+        case 4: //识别三岔、第二个十字回环出口
+        {
+            flag=5; //三岔从车库这边进入不太稳定
+            break;
+        }
+        case 5://识别三岔和右车库
+        {
+            gpio_set(P21_4, 1);
+            if(LostNum_RightLine>40 && LostNum_RightLine<90 && LostNum_LeftLine<10 && LostNum_LeftLine>0)
+            {
+                Garage_flag=GarageIdentify('R', LeftDownPoint, RightDownPoint);//识别车库
+                if(Garage_flag==0)//如果没识别到车库，再继续识别三岔，怕误判,补救
+                {
+                    Fork_flag=ForkIdentify(LeftLine, RightLine, LeftDownPoint, RightDownPoint);
+                }
+            }
+            else
+            {
+                Fork_flag=ForkIdentify(LeftLine, RightLine, LeftDownPoint, RightDownPoint);
+            }
+        }
+    }
     /***************************偏差计算**************************/
     Bias=DifferentBias(100,60,CentreLine);//无特殊处理时的偏差计算
 }
