ba2cb9d74a3ce27fe26207d8250c78cfb2bb15e9
hao-yue-1 <1801134420@qq.com>
2022-08-13 16:34:44 +0800

    修改舵机中值

--- Seekfree_TC264_Opensource_Library/CODE/Steer.h
+++ Seekfree_TC264_Opensource_Library/CODE/Steer.h
@@ -11,9 +11,9 @@
 #define CODE_STEER_H_
 
 #define STEER_PIN   ATOM1_CH1_P33_9     //输出舵机PWM引脚
-#define STEER_MID   762                 //舵机居中值
-#define STEER_LEFT  840                 //舵机向左打死值
-#define STEER_RIGHT 685                 //舵机向右打死值
+#define STEER_MID   771                 //舵机居中值
+#define STEER_LEFT  849                 //舵机向左打死值
+#define STEER_RIGHT 690                 //舵机向右打死值
 
 void SteerCtrl(uint32 pwm);     //控制舵机
 
