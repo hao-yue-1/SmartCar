53f5857246c780cc1d7bff74ca8ea8b95411f70b
hao-yue-1 <1801134420@qq.com>
2022-08-19 13:32:52 +0800

    修改添加两个调参按键后的Bug

--- Seekfree_TC264_Opensource_Library/CODE/ImageProcess.h
+++ Seekfree_TC264_Opensource_Library/CODE/ImageProcess.h
@@ -13,6 +13,9 @@
 #include "ImageTack.h"      //计算偏差、补线
 #include "ImageSpecial.h"   //元素识别
 
+#define PROCESS_SPEED_LEN   20
+#define PROCESS_ENCODER_LEN 5
+
 extern uint8 CrossRoads_flag;        //十字标志变量
 extern uint8 Fork_flag;              //三岔识别的标志变量
 extern uint8 Circle_flag;
@@ -20,6 +23,10 @@ extern uint8 Garage_flag;
 extern int16 speed_case_1,speed_case_2,speed_case_3,speed_case_4,speed_case_5,speed_case_6,speed_case_7;
 extern uint8 bias_startline,bias_endline;        //动态前瞻
 extern uint8 process_flag;
+
+extern uint8 process_status[20];
+extern uint16 process_speed[PROCESS_SPEED_LEN];
+extern uint8 process_encoder[PROCESS_ENCODER_LEN];
 
 void ImageProcess(void);//图像处理逻辑函数
 void Stop(void);        //停车
